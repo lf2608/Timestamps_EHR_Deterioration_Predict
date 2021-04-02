cc356055779824f733f3fb6c14321fa902dc05e0
LIHENG FU <43299707+lf2608@users.noreply.github.com>
2021-04-02 16:10:45 -0400

    Update modeling_pipeline_single_point.py
    Added baseline model

--- Source_code/modeling_pipeline_single_point.py
+++ Source_code/modeling_pipeline_single_point.py
@@ -1,55 +1,59 @@
 import matplotlib.pyplot as plt
 import seaborn as sns;
+from operator import add
+from functools import reduce
+from tqdm import tqdm
 
 sns.set()
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.dummy import DummyClassifier
 from sklearn.model_selection import GridSearchCV
 from sklearn.utils import resample, shuffle
 
 from Source_code.data_preprocessing_pipeline import select_column
-from Source_code.plot_metrics import plot_cm, plot_pr_curve, plot_roc
+from Source_code.plot_metrics import metrics, plot_cm, plot_pr_curve, plot_roc
 
 
-LR_model = LogisticRegression(random_state=0)
+LR_model = LogisticRegression()
 LR_paragram_grid = [{'penalty': ['l1', 'l2'], 'C': [10 ** n for n in range(-4, 5)],
                      'solver': ['liblinear']},
                     {'penalty': ['none'], 'solver': ['newton-cg']}]
 
-RF_model = RandomForestClassifier(random_state=0)
+RF_model = RandomForestClassifier()
 RF_paragram_grid = [{'n_estimators': [50, 100, 200], 'max_depth': [20, 50, None],
                      'min_samples_split': [2, 5, 10], 'min_samples_leaf': [1, 2, 5]}]
 
 
-LR_best_model = LogisticRegression(random_state=1, penalty='l2', C=100, solver='liblinear')
-RF_best_model = RandomForestClassifier(random_state=1, n_estimators=50, min_samples_leaf=1, min_samples_split=2,
+LR_best_model = LogisticRegression(penalty='l2', C=1, solver='liblinear')
+RF_best_model = RandomForestClassifier(random_state=1, n_estimators=100, min_samples_leaf=1, min_samples_split=2,
                                        max_depth=50)
+baseline_model = DummyClassifier(strategy='most_frequent')
+baseline_paragram_grid=[]
 
-
-def get_results():
+def get_results(directory, sampling, time_of_day, calculate_CI=False):
     results = []
-    time_of_day = [True, False]
-    sampling = ['last']
-    algorithm_list = ['Logistic_Regression', 'Random_Forest']
+    algorithm_list = ['Logistic_Regression', 'Random_Forest', 'Baseline']
     column = ['AUROC', 'AUPRC', 'sensibility', 'specificity', 'PPV', 'NPV', 'FScore']
-    thresholds = [0.6, 0.1]
-    algorithms = {'Logistic_Regression': LR_best_model, 'Random_Forest': RF_best_model}
-    paragram_grids = {'Logistic_Regression': LR_paragram_grid, 'Random_Forest': RF_paragram_grid}
+
+    thresholds = [0.6, 0.1, 0.5]
+    algorithms = {'Logistic_Regression': LR_best_model, 'Random_Forest': RF_best_model, 'Baseline': baseline_model}
+    paragram_grids = {'Logistic_Regression': LR_paragram_grid, 'Random_Forest': RF_paragram_grid, 'Baseline': baseline_paragram_grid}
     for algorithm, threshold in zip(algorithm_list, thresholds):
         for method in sampling:
             for time in time_of_day:
-                x = BuildAlgorithmsSinglePoint(sampling_method=method, timestep_length=60,
+                x = BuildAlgorithmsSinglePoint(directory=directory, sampling_method=method, timestep_length=60,
                                                algorithm=algorithms[algorithm], paragram_grid=paragram_grids[algorithm],
                                                time_of_day=time)
-                results.append(list(x.get_results(search=False, threshold=threshold)))
+                results.append(list(x.get_results(search=False, threshold=threshold, calculate_CI=calculate_CI)))
     return pd.DataFrame(results, columns=column,
                         index=pd.MultiIndex.from_product([algorithm_list, sampling, time_of_day]))
 
 
 class BuildAlgorithmsSinglePoint(object):
-    _DIRECTORY = '/home/user/dataset/'
+    _DIRECTORY = '/home/liheng/Mat/Source_code/dataset_sum/'
 
     def __init__(self, directory=None, sampling_method='random', timestep_length=60, algorithm=None, paragram_grid=None,
                  vitals=True, v_order=True, med_order=True, comments=True, notes=True, time_of_day=True):
@@ -79,19 +83,37 @@ class BuildAlgorithmsSinglePoint(object):
             best_model = model_training(self.__algorithm, point_train_data, train_label)
         return best_model
 
-    def get_results(self, search=False, threshold=0.5):
+    def get_results(self, search=False, threshold=0.5, calculate_CI=False):
         _, _, _, point_test_data, _, test_label = self._load_data(self.__method, self.__length)
         point_test_data = self._standardize_numeric_col(point_test_data)
         point_test_data = \
             self._feature_selection(ds=point_test_data, vitals=self.__vitals, v_order=self.__v_order,
                                     med_order=self.__med_order, comments=self.__comments,
                                     notes=self.__notes, time_of_day=self.__time_of_day)
-        best_model = self.get_best_model(search)
-        AUROC, AUPRC, sensibility, specificity, PPV, NPV, FScore \
-            = model_validation(best_model, point_test_data, test_label, threshold)
-        print('AUROC:{}, AUPRC:{}, sensibility:{}, specificity:{}, PPV:{}, NPV:{}, F-Score:{}'
-              .format(AUROC, AUPRC, sensibility, specificity, PPV, NPV, FScore))
-        return AUROC, AUPRC, sensibility, specificity, PPV, NPV, FScore
+        if calculate_CI:
+            AUROC_list, AUPRC_list, sensibility_list, specificity_list, PPV_list, NPV_list, FScore_list \
+                = list(), list(), list(), list(), list(), list(), list()
+            variables = [AUROC_list, AUPRC_list, sensibility_list, specificity_list, PPV_list, NPV_list, FScore_list]
+
+            def append_list(variables, values):
+                for var, val in zip(variables, values):
+                    var.append(val)
+
+            for i in tqdm(list(range(100))):
+                best_model = self.get_best_model(search)
+                AUROC, AUPRC, sensitivity, specificity, PPV, NPV, FScore \
+                    = model_validation(best_model, point_test_data, test_label, threshold)
+                values = [AUROC, AUPRC, sensitivity, specificity, PPV, NPV, FScore]
+                append_list(variables, values)
+            AUROC, AUPRC, sensitivity, specificity, PPV, NPV, FScore \
+                = list(map(self._calculate_CI, variables))
+        else:
+            best_model = self.get_best_model(search)
+            AUROC, AUPRC, sensitivity, specificity, PPV, NPV, FScore \
+                = model_validation(best_model, point_test_data, test_label, threshold)
+        print('AUROC:{}, AUPRC:{}, sensitivity:{}, specificity:{}, PPV:{}, NPV:{}, F-Score:{}'
+              .format(AUROC, AUPRC, sensitivity, specificity, PPV, NPV, FScore))
+        return AUROC, AUPRC, sensitivity, specificity, PPV, NPV, FScore
 
     def _load_data(self, sampling_method, timestep_length):
         directory = self.__directory
@@ -140,12 +162,12 @@ class BuildAlgorithmsSinglePoint(object):
 
     def _oversampling(self, train_data, train_label):
         pos_data, pos_label = resample(train_data[train_label == 1], train_label[train_label == 1],
-                                       n_samples=(train_label == 0).sum(), replace=True, random_state=0)
+                                       n_samples=(train_label == 0).sum(), replace=True)
         neg_data = train_data[train_label == 0]
         neg_label = train_label[train_label == 0]
         new_data = np.concatenate((pos_data, neg_data))
         new_label = np.concatenate([pos_label, neg_label], axis=None)
-        new_data, new_label = shuffle(new_data, new_label, random_state=0)
+        new_data, new_label = shuffle(new_data, new_label)
         return new_data, new_label
 
     def _column_name(self, freq):
@@ -154,6 +176,13 @@ class BuildAlgorithmsSinglePoint(object):
         col = select_column(base=False, vitals=True, v_order=True, med_order=True, comments=True, notes=True)
         col += ['Time_of_day_' + str(t)[7:] for t in pd.timedelta_range(0, periods=periods, freq=freq)]
         return col
+
+    def _calculate_CI(self, value_list, alpha=0.95):
+        p = (1-alpha)/2*100
+        lower = np.percentile(value_list, p).round(3)
+        upper = np.percentile(value_list, 100-p).round(3)
+        mean = np.mean(value_list).round(3)
+        return '{} ({}, {})'.format(mean, lower, upper)
 
 
 def model_searching(algorithm, gridsearch_dict, training_data, training_label):
@@ -170,14 +199,14 @@ def model_training(best_model, training_data, training_label):
     return best_model
 
 
-def model_validation(model, test_data, true_label, threshold):
+def model_validation(model, test_data, true_label, threshold, plot=False):
     pred_label = model.predict_proba(test_data)[:, 1]
-
-    sensibility, specificity, PPV, NPV = plot_cm(true_label, pred_label, p=threshold)
-    AUPRC = plot_pr_curve('AUPRC', true_label, pred_label)
-    AUROC = plot_roc('AUROC', true_label, pred_label)
-    FScore = 2 / (sensibility ** -1 + PPV ** -1)
+    if plot:
+        sensibility, specificity, PPV, NPV = plot_cm(true_label, pred_label, p=threshold)
+        AUPRC = plot_pr_curve('AUPRC', true_label, pred_label)
+        AUROC = plot_roc('AUROC', true_label, pred_label)
+        FScore = 2 / (sensibility ** -1 + PPV ** -1)
+    else:
+        AUROC, AUPRC, sensibility, specificity, PPV, NPV, FScore = metrics(true_label, pred_label, p=threshold)
     return AUROC, AUPRC, sensibility, specificity, PPV, NPV, FScore
-
-
 
